7e57f01985f08b03b81ba1d4aedb891744f646e6
Ahmed Yassine Messaoudi <hammoudamessaoudi123@gmail.com>
2026-04-19 19:17:07 -0700

    Add getters and setters for Rental class properties

--- src/domain/Rental.java
+++ src/domain/Rental.java
@@ -22,5 +22,43 @@ public class Rental {
         this.totalCost = totalCost;
     }
 
-    // Getters and Setters
+    public int getRentalId() {
+        return rentalId;
+    }
+
+    public void setRentalId(int rentalId) {
+        this.rentalId = rentalId;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public List<Equipment> getEquipmentList() {
+        return equipmentList;
+    }
+
+    public void setEquipmentList(List<Equipment> equipmentList) {
+        this.equipmentList = equipmentList;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
 }
