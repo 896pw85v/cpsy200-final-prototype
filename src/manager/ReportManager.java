a6848f27732a463b4bb1f2cd2ebe74cfc9b8ae29
Jon Ibarlin <Jon.Ibarlin@edu.sait.ca>
2026-04-19 20:30:47 -0600

    ReportManager Addition

--- src/manager/ReportManager.java
+++ src/manager/ReportManager.java
@@ -1,22 +1,59 @@
 package manager;
 
+import domain.Equipment;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class ReportManager {
 
     public void salesByDate(Date date) {
-        // TODO: implement
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("\n==========================================");
+        System.out.println(" SALES REPORT FOR DATE: " + sdf.format(date));
+        System.out.println("==========================================");
+        System.out.println("Data would be aggregated from Rentals...");
+        System.out.println("==========================================\n");
     }
 
     public void salesByCustomer(int id) {
-        // TODO: implement
+        System.out.println("\n==========================================");
+        System.out.println(" SALES REPORT FOR CUSTOMER ID: " + id);
+        System.out.println("==========================================");
+        System.out.println("Data would be aggregated from Rentals...");
+        System.out.println("==========================================\n");
     }
 
     public void listByCategory(int categoryId) {
-        // TODO: implement
+        System.out.println("\n==========================================");
+        System.out.println(" EQUIPMENT IN CATEGORY ID: " + categoryId);
+        System.out.println("==========================================");
+        
+        EquipmentManager em = new EquipmentManager();
+        List<Equipment> equipment = em.listEquipment();
+        boolean found = false;
+        
+        if (equipment != null && !equipment.isEmpty()) {
+            for (Equipment e : equipment) {
+                if (e.getCategoryId() == categoryId) {
+                    System.out.println("ID: " + e.getEquipmentId() + " | Name: " + e.getName() + 
+                                       " | Cost: $" + e.getDailyCost() + " | Status: " + e.getStatus());
+                    found = true;
+                }
+            }
+        }
+        
+        if (!found) {
+            System.out.println("No equipment currently found for this category.");
+        }
+        System.out.println("==========================================\n");
     }
 
     public void customReport(Object params) {
-        // TODO: implement
+        System.out.println("\n==========================================");
+        System.out.println(" CUSTOM REPORT ");
+        System.out.println("==========================================");
+        System.out.println("Generating report with parameters: " + params.toString());
+        System.out.println("==========================================\n");
     }
 }
